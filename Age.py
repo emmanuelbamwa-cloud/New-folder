03404a37ec14451b2e18ac824cee44f9b98349f8
Emmanuel <emmanuelbamwa@gmail.com>
2026-07-02 11:27:42 +0100

    loops tasks

---
+++ Age.py
@@ -0,0 +1,8 @@
+def main():
+    yr = int(input("Year of birth: "))
+
+    age = 2026 - yr
+
+    print(f"You are {age} years old")
+
+main()
---
+++ club.py
@@ -0,0 +1,5 @@
+#Ask user for their favorite club
+
+club = input("Club name: ").strip().title()
+
+print(f"Your favorite club is {club}!")
